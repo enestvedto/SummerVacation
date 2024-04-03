d2404eb674bce89080b06cd9517366c2c36d27fe
Owen Enestvedt <enestvedto@merrimack.edu>
2024-04-03 09:13:33 -0400

    Created a config file to hide username/password

---
+++ src/ConfigReader.java
@@ -0,0 +1,24 @@
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class ConfigReader {
+    private Properties properties;
+
+    public ConfigReader() {
+        properties = new Properties();
+        try (FileInputStream fis = new FileInputStream("config.properties")) {
+            properties.load(fis);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String getUsername() {
+        return properties.getProperty("username");
+    }
+
+    public String getPassword() {
+        return properties.getProperty("password");
+    }
+}
